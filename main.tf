da78a722750eb4c6496a17ac9f309b43f79b2d6a
Denise Yu <denise.yu@hashicorp.com>
2025-09-19 16:02:16 -0400

    add AWS provider

--- main.tf
+++ main.tf
@@ -7,4 +7,18 @@ terraform {
       name = "terraform-testing" 
     } 
   } 
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
